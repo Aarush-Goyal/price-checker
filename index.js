1526a9c4b6cbf5981a8f2f9bdf5bc93c23324d16
Aarush-Goyal <goyalaarush92@gmail.com>
2021-02-14 03:26:35 +0530

    added the target price option

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import nodemailer from "nodemailer";
 const url =
   "https://www.amazon.in/Talgo-7888007-Analogue-Basics-Men/dp/B084HFRRKM/ref=sr_1_3?crid=3SGM4QUV989IW&dchild=1&keywords=crony+watches+for+men&qid=1613239665&sprefix=cron%2Caps%2C382&sr=8-3";
 
+const targetPrice = 400;
+
 async function configureBrowser() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -22,7 +24,12 @@ async function checkPrice(page) {
     let price = $(this).text();
     // console.log(price);
     let currentPrice = Number(price.replace(/[^0-9.-]+/g, ""));
-    console.log(currentPrice);
+
+    console.log(
+      `The current price is ${currentPrice} \nYou can ${
+        currentPrice < targetPrice ? "Buy" : "Sell"
+      } it!`
+    );
   });
 }
 
